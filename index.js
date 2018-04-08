9a0b98a794e8d1cbfd968c4ba26caa64b4466942
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-08 04:31:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function nowServing (deliLine){
 
 function currentLine (katzDeliLine){
   var line = katzDeliLine;
+<<<<<<< HEAD
   var i;
 
   if (katzDeliLine.length === 0){
@@ -36,4 +37,17 @@ function currentLine (katzDeliLine){
     }
     return "The line is currently: " + response;
   }
+=======
+
+  if (katzDeliLine === undefined){
+    return "The line is currently empty."
+  }else{
+    for (var i = 0; i < line.length; i++){
+      // var position = line.indexOf(line[i]);
+      // var person = line[i];
+      return "The line is currently: " + (++(line.indexOf(line[i]))) + "." + " " + line[i] +",";
+    }
+  }
+
+>>>>>>> f1d70b32aa982fe85332d6e6628d287dc722f799
 }
